5498699d218c0139304aec2f6150da6e8a59d4d3
dipak1920basnet <dipak1920basnet@gmail.com>
2025-03-16 14:43:47 +0545

    Convert Markdown content

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
 
 from . import util
 
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def call(request, name):
+    content = util.get_entry(name)
+    if content == None:
+        return render(request, "encyclopedia/error.html",
+                      {
+                          "message":f" requested page {name} was not found. "
+                      })
+    return render(request, "encyclopedia/entry.html",{
+        "title":name,
+        "entrie":markdown2.Markdown().convert(content)
+    })
